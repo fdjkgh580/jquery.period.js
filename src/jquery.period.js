157d682cda41797bc65f6ee2901532f66b220615
fdjkgh580 <fdjkgh580@yahoo.com.tw>
2018-07-24 16:00:33 +0800

    改用多重參數傳遞 增加 before() and after()

--- src/jquery.period.js
+++ src/jquery.period.js
@@ -1,40 +1,69 @@
 (function ( $ ) {
 
-    /**
-     * 批次讓元素間隔時間執行
-     * @param  {int}   delayTimes 延遲的時間
-     * @param  {callable} callback   對物件執行的動作
-     */
-    $.fn.period = function (delayTimes, callback){
-        
-        var _total      = $(this).length,
-            _collection = $(this),
-            _delayTimes = delayTimes,
-            _callback   = callback;
+    let _total,
+        _collection,
+        _delayTimes,
+        _before,
+        _todo,
+        _after;
 
-        var loop = function (startIndex){
+    let _endLoop;
 
-            // 取得元素
-            var target = _collection.get(startIndex);
+    const loop = (startIndex) => {
 
-            // 執行回呼，當等於 false 將會中斷前往下一個元素
-            var result = _callback.call(target, startIndex);
+        // 取得元素
+        var target = _collection.get(startIndex);
 
-            if (result === false) return false;
+        // 執行回呼，當等於 false 將會中斷前往下一個元素
+        var result = _todo.call(target, startIndex);
 
-            var nextIndex = startIndex + 1;
-
-            // 下一個元素不存在則終止
-            if ($(_collection.get(nextIndex)).length == 0) return false;
-
-            setTimeout(function (){
-                
-                loop(startIndex + 1)
-            
-            }, _delayTimes)
+        // 當回傳得到 false 會終止，並觸發 after()
+        if (result === false) {
+            _endLoop.call(target);
+            return false;
         }
 
+        var nextIndex = startIndex + 1;
+
+        // 下一個元素不存在則終止
+        if ($(_collection.get(nextIndex)).length == 0) {
+            _endLoop.call(target);
+            return false;
+        }
+
+        setTimeout(() => {
+            loop(startIndex + 1)
+        }, _delayTimes)
+    }
+
+
+    /**
+     * 批次讓元素間隔時間執行
+     * @param  {int}        param.delayTimes     延遲的時間
+     * @param  {callable}   before               執行前
+     * @param  {callable}   todo                 指派動作
+     * @param  {callable}   after                結束後
+     */
+    $.fn.period = function (param){
+        
+        _total      = $(this).length,
+        _collection = $(this),
+        _delayTimes = param.delayTimes,
+        _before     = param.before,
+        _todo       = param.todo,
+        _after      = param.after;
+
+        
+        // 觸發前
+        _before.call(this);
+
+        // 循環觸發
         loop(0);
+
+        // 當 loop 停止的時候觸發 after()
+        _endLoop = () => {
+            _after.call(this);
+        }
     }
 
 
